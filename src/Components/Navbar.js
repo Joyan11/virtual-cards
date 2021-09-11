ae781ecef989ebec072fbf45afe93e683c8fd7d7
joyan11 <joyan.s03@gmail.com>
2021-09-11 09:19:26 +0530

    chore: added new elements

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,13 +2,26 @@
 import { BiMenu } from "react-icons/bi";
 import { BsFillGridFill } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
 import { useMainContext } from "../context/MainContext";
+import { AiOutlineVideoCamera, AiOutlinePlus } from "react-icons/ai";
 
 export const Navbar = () => {
   const { dispatch } = useMainContext();
   return (
     <nav className="w-full ">
+      <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row justify-between px-5 md:px-24 py-2 items-start md:items-center flex-wrap">
+        <div className="flex items-center space-x-2">
+          <h1 className="text-2xl font-extrabold">Virtual Cards</h1>
+          <span className="flex items-center text-blue-400 font-semibold bg-gray-100 rounded px-1 space-x-1 cursor-pointer">
+            <AiOutlineVideoCamera />
+            <span>Learn more</span>
+          </span>
+        </div>
+        <button className="self-end flex items-center space-x-3 px-3 py-1 font-semibold shadow-md">
+          <AiOutlinePlus />
+          <span>Virtual Card</span>
+        </button>
+      </div>
       <div className="flex justify-between px-5 md:px-24 py-3 border-solid border-coolgray-300 border-b-2 items-center">
         <div className="flex space-x-3 text-gray-400 text-1xl items-center">
           <NavLink to="/mycards" activeClassName="active" end>
