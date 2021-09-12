d40924cceed81873e94933b27237f03d9ee9934c
joyan11 <joyan.s03@gmail.com>
2021-09-12 13:09:40 +0530

    refactor: index file for universal import

---
+++ src/Components/AllCards/index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import { FilterButton } from "./FilterButton";
+import { FilterModal } from "./FilterModal";
+import { NoCardsFound } from "./NoCardsFound";
+import { Search } from "./Search";
+
+export { FilterButton, FilterModal, NoCardsFound, Search };
