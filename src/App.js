aa56e5415c126f2a5d40ab9cf214aa82ca233406
joyan11 <joyan.s03@gmail.com>
2021-09-12 13:10:13 +0530

    refactor: removed unnessary imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 /** @format */
 
 import "./App.css";
-import { useMainContext } from "./context/MainContext";
-import apiData from "./data/CardData.json";
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./Components/Navbar";
 import { AllCards } from "./pages/AllCards";
 import { YourCard } from "./pages/YoursCard";
 import { BlockedCards } from "./pages/BlockedCards";
-import { Card } from "./Components/Card";
 
 function App() {
   return (
