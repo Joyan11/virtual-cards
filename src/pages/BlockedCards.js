ce0883d905b22c3b19b0f1b17ad2d020b3044677
joyan11 <joyan.s03@gmail.com>
2021-09-12 13:07:39 +0530

    style: layout changes

--- src/pages/BlockedCards.js
+++ src/pages/BlockedCards.js
@@ -1,24 +1,27 @@
 /** @format */
 
+import React from "react";
 import { Card } from "../Components/Card";
 import { useMainContext } from "../context/MainContext";
 
 export const BlockedCards = () => {
   const { allData, view } = useMainContext();
 
-  const BlockedCards = allData?.data.filter(
+  const blockedCards = allData?.filter(
     (details) => details.status === "blocked"
   );
 
-  const renderedCards = BlockedCards.map((details) => (
-    <Card details={details} />
-  ));
+  const renderedCards = React.Children.toArray(
+    blockedCards.map((details) => (
+      <div className=" m-2 md:m-5 w-full max-w-xl">
+        <Card details={details} />
+      </div>
+    ))
+  );
 
   return (
     <section
-      className={`mt-2 md:mt-4 flex flex-wrap justify-center items-center ${
-        view === "list" ? "flex-col" : "flex-row"
-      }`}>
+      className={`card-page ${view === "list" ? "flex-col" : "flex-row"}`}>
       {renderedCards}
     </section>
   );
--- src/pages/YoursCard.js
+++ src/pages/YoursCard.js
@@ -1,22 +1,27 @@
 /** @format */
 
+import React from "react";
 import { Card } from "../Components/Card";
 import { useMainContext } from "../context/MainContext";
 
 export const YourCard = () => {
   const { allData, currentUserid, view } = useMainContext();
 
-  const myCards = allData?.data.filter(
+  const myCards = allData?.filter(
     (details) => details.owner_id === currentUserid
   );
 
-  const renderedCards = myCards.map((details) => <Card details={details} />);
+  const renderedCards = React.Children.toArray(
+    myCards.map((details) => (
+      <div className=" m-2 md:m-5 w-full max-w-xl">
+        <Card details={details} />
+      </div>
+    ))
+  );
 
   return (
     <section
-      className={`mt-2 md:mt-4 flex flex-wrap justify-center items-center ${
-        view === "list" ? "flex-col" : "flex-row"
-      }`}>
+      className={`card-page ${view === "list" ? "flex-col" : "flex-row"}`}>
       {renderedCards}
     </section>
   );
