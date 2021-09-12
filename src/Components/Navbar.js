7020ddddb43aaefc7a6f1b36b5c0d52d08348351
joyan11 <joyan.s03@gmail.com>
2021-09-12 13:09:26 +0530

    chore: new elements

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,8 +9,8 @@ export const Navbar = () => {
   const { dispatch } = useMainContext();
   return (
     <nav className="w-full ">
-      <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row justify-between px-5 md:px-24 py-2 items-start md:items-center flex-wrap">
-        <div className="flex items-center space-x-2">
+      <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row justify-between px-5 md:px-20 py-2 items-start md:items-center flex-wrap">
+        <div className="flex items-center space-x-2 pt-2">
           <h1 className="text-2xl font-extrabold">Virtual Cards</h1>
           <span className="flex items-center text-blue-400 font-semibold bg-gray-100 rounded px-1 space-x-1 cursor-pointer">
             <AiOutlineVideoCamera />
@@ -22,7 +22,7 @@ export const Navbar = () => {
           <span>Virtual Card</span>
         </button>
       </div>
-      <div className="flex justify-between px-5 md:px-24 py-3 border-solid border-coolgray-300 border-b-2 items-center">
+      <div className="flex justify-between px-5 md:px-20 py-3 border-solid border-coolgray-300 border-b-2 items-center">
         <div className="flex space-x-3 text-gray-400 text-1xl items-center">
           <NavLink to="/mycards" activeClassName="active" end>
             Your
